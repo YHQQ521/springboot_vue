1eeab8deb6a905a067daf1b7a45480e0bd6cca84
19036369 <853560187@qq.com>
2020-03-16 10:43:34 +0800

    新增服务端和管理后台

--- yhqq/src/main/java/com/blog/yhqq/repository/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.blog.yhqq.repository.mapper;
-
-import com.blog.yhqq.domain.User;
-
-import java.util.List;
-
-public interface UserMapper {
-    List<User> queryAllUsers();
-}
--- yhqq/src/main/java/com/blog/yhqq/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.blog.yhqq.service;
-
-import com.blog.yhqq.domain.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> queryAllUsers();
-}
